250a99e57bcca4c60ebd24f969ba5e9414252757
zelly-log <dearmyjaei@gmail.com>
2025-03-26 16:48:54 +0900

    chore: Create Spring Boot Project

---
+++ src/main/java/com/ll/web3_4_frend_be/Web34FrEndBeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.web3_4_frend_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web34FrEndBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web34FrEndBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/web3_4_frend_be/Web34FrEndBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.web3_4_frend_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web34FrEndBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

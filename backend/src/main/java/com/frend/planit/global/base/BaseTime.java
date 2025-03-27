cf770e49d1ffc0446fe0c6c5433985169d981ba9
HJ LEE <sete3683@gmail.com>
2025-03-28 01:35:49 +0900

    [FEAT] 기본 엔티티 추가 및 프로젝트 구조 수정
    - BaseEntity, BaseTime 추가
    - 요구사항 변경으로 인한 프로젝트 구조 수정

---
+++ backend/src/main/java/com/frend/planit/global/base/BaseTime.java
@@ -0,0 +1,25 @@
+package com.frend.planit.global.base;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseTime extends BaseEntity {
+
+    @CreatedDate
+    @Setter(AccessLevel.PRIVATE)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Setter(AccessLevel.PRIVATE)
+    private LocalDateTime modifiedAt;
+}

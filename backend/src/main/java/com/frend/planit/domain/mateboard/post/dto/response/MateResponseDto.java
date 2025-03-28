eeb2d9e576b4e5cc100fabf2b79806b8fe33d684
zelly-log <dearmyjaei@gmail.com>
2025-03-28 17:02:24 +0900

    [Chore] 병합 충돌 해결 커밋

--- backend/src/main/java/com/frend/planit/domain/mateboard/post/dto/response/MateResponseDto.java
+++ backend/src/main/java/com/frend/planit/domain/mateboard/post/dto/response/MateResponseDto.java
@@ -5,8 +5,6 @@ import com.frend.planit.domain.mateboard.post.entity.RecruitmentStatus;
 import com.frend.planit.domain.mateboard.post.entity.TravelRegion;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import lombok.Builder;
-import lombok.Getter;
 
 /**
  * 메이트 모집 게시글 조회, 목록 응답 시에 사용하는 Response DTO입니다.
@@ -15,11 +13,9 @@ import lombok.Getter;
  * @version 1.0
  * @since 2025-03-28
  */
-@Getter
-@Builder
 public class MateResponseDto {
 
-    private Long id;
+    private Long matePostId;
 
     private String title;
 
@@ -36,5 +32,4 @@ public class MateResponseDto {
     private MateGender mateGender;
 
     private LocalDateTime createdAt;
-
 }

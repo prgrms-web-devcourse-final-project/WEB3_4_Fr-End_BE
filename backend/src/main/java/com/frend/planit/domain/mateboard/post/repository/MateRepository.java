5868691dfd1427871142d1d4a27a3a470dffe4b4
zelly-log <dearmyjaei@gmail.com>
2025-03-29 00:37:11 +0900

    [FEAT] Add MateRepository

---
+++ backend/src/main/java/com/frend/planit/domain/mateboard/post/repository/MateRepository.java
@@ -0,0 +1,17 @@
+package com.frend.planit.domain.mateboard.post.repository;
+
+import com.frend.planit.domain.mateboard.post.entity.Mate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 메이트 모집 게시글에 대한 데이터베이스 접근을 처리하는 Repository 인터페이스입니다.
+ * <p> 기본 CRUD 메서드는 JpaRepository에서 상속받아 사용할 수 있으며,</p>
+ * <p> 추후 조건 검색 등 커스텀 쿼리 메서드도 이 인터페이스에 정의합니다.</p>
+ *
+ * @author zelly
+ * @version 1.0
+ * @since 2025-03-28
+ */
+public interface MateRepository extends JpaRepository<Mate, Long> {
+
+}

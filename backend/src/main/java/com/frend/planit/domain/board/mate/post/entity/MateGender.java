3e5a6a82de17fb1c20cfb5a1436ea0eca54d0044
zelly-log <dearmyjaei@gmail.com>
2025-03-28 15:34:53 +0900

    [Refactor] Add 성별 무관(NO_PREFERENCE) to MateGender

--- backend/src/main/java/com/frend/planit/domain/board/mate/post/entity/MateGender.java
+++ backend/src/main/java/com/frend/planit/domain/board/mate/post/entity/MateGender.java
@@ -1,7 +1,7 @@
 package com.frend.planit.domain.board.mate.post.entity;
 
 /**
- * 모집 메이트의 성별을 정의하는 Enum입니다. 여성(FEMALE), 남성(MALE) 2가지 항목이 존재합니다.
+ * 모집 메이트의 성별을 정의하는 Enum입니다. 여성(FEMALE), 남성(MALE), 성별 무관(NO_PREFERENCE) 3가지 항목이 존재합니다.
  *
  * @author zelly
  * @version 1.0
@@ -9,9 +9,10 @@ package com.frend.planit.domain.board.mate.post.entity;
  */
 public enum MateGender {
 
+    NO_PREFERENCE("성별 무관"),
     FEMALE("여성"),
     MALE("남성");
-
+    
     private final String displayName;
 
     MateGender(String displayName) {

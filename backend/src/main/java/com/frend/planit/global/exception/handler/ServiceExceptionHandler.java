7f35a536c5fb4b614ec6766d9f21018bb0003d56
sete3683 <108328000+sete3683@users.noreply.github.com>
2025-03-31 10:37:31 +0900

    Feature/G-05 : 더욱 다양한 예외 처리 기능 추가 (#33)
    이하 Squash
    
    * [FEAT] 유효성 검사 관련 예외 처리 핸들러 작성
    
    - Issue #31
    
    * [Test] 예외 데이터 업데이트 및 예외 핸들러 테스트 코드 작성
    
    - 핸들러 우선순위 지정
    - 예외 처리 추가
    - 테스트 코드 작성
    - Issue #31

--- backend/src/main/java/com/frend/planit/global/exception/handler/ServiceExceptionHandler.java
+++ backend/src/main/java/com/frend/planit/global/exception/handler/ServiceExceptionHandler.java
@@ -1,15 +1,18 @@
-package com.frend.planit.global.exception;
+package com.frend.planit.global.exception.handler;
 
+import com.frend.planit.global.exception.ServiceException;
 import com.frend.planit.global.response.ApiResponseHelper;
 import com.frend.planit.global.response.ErrorResponse;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /*
  * 서비스 관련 예외 처리를 위한 핸들러 클래스입니다.
  */
-@ControllerAdvice
+@Order(1)
+@RestControllerAdvice
 public class ServiceExceptionHandler {
 
     @ExceptionHandler(ServiceException.class)
--- backend/src/main/java/com/frend/planit/global/response/ErrorResponse.java
+++ backend/src/main/java/com/frend/planit/global/response/ErrorResponse.java
@@ -1,7 +1,7 @@
 package com.frend.planit.global.response;
 
+import java.util.List;
 import lombok.Getter;
-import lombok.NonNull;
 
 /*
  * 에러 응답을 위한 DTO 클래스입니다.
@@ -11,8 +11,35 @@ import lombok.NonNull;
 public class ErrorResponse {
 
     private final String message;
+    private final List<Detail> details;
 
-    public ErrorResponse(@NonNull String message) {
+    public ErrorResponse(String message) {
         this.message = message;
+        this.details = null;
+    }
+
+    public ErrorResponse(String message, List<Detail> details) {
+        this.message = message;
+        this.details = details;
+    }
+
+    @Getter
+    public static class Detail {
+
+        private final String field;
+        private final Object value;
+        private final String message;
+
+        public Detail(String field, String message) {
+            this.field = field;
+            this.value = null;
+            this.message = message;
+        }
+
+        public Detail(String field, Object value, String message) {
+            this.field = field;
+            this.value = value;
+            this.message = message;
+        }
     }
 }

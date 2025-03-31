5f3795671bbec2b659a2abf1eebfe08f27865644
sete3683 <108328000+sete3683@users.noreply.github.com>
2025-03-31 10:37:31 +0900

    Feature/G-05 : 더욱 다양한 예외 처리 기능 추가 (#33)
    이하 Squash
    
    * [FEAT] 유효성 검사 관련 예외 처리 핸들러 작성
    
    - Issue #31
    
    * [Test] 예외 데이터 업데이트 및 예외 핸들러 테스트 코드 작성
    
    - 핸들러 우선순위 지정
    - 예외 처리 추가
    - 테스트 코드 작성
    - Issue #31

--- backend/src/main/java/com/frend/planit/global/response/ErrorType.java
+++ backend/src/main/java/com/frend/planit/global/response/ErrorType.java
@@ -2,7 +2,6 @@ package com.frend.planit.global.response;
 
 import com.frend.planit.global.validation.CodeValidation;
 import lombok.Getter;
-import lombok.NonNull;
 import org.springframework.http.HttpStatus;
 
 /*
@@ -19,20 +18,34 @@ public enum ErrorType {
      * 에러 코드이므로 2xx 범위의 코드는 지정할 수 없습니다.
      */
     EXAMPLE_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
-    EXAMPLE_CUSTOM_ERROR(444, "오류가 발생했습니다.");
+    EXAMPLE_CUSTOM_ERROR(444, "오류가 발생했습니다."),
+
+    /*********************************************************************************/
+
+    // Global
+    ARGUMENT_BINDING_ERROR(HttpStatus.BAD_REQUEST, "요청 본문의 값이 잘못되었습니다."),
+    CONSTRAINT_VIOLATION(HttpStatus.BAD_REQUEST, "요청 파라미터의 값이 잘못되었습니다."),
+    ARGUMENT_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "요청 파라미터의 타입이 잘못되었습니다."),
+    MISSING_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "필수 요청 파라미터가 누락되었습니다."),
+    MISSING_PATH_VARIABLE(HttpStatus.INTERNAL_SERVER_ERROR, "경로 파라미터가 누락되었습니다."),
+    JSON_NOT_READABLE(HttpStatus.BAD_REQUEST, "요청 본문의 형식이 잘못되었습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메소드입니다."),
+    NO_RESOURCE_FOUND(HttpStatus.NOT_FOUND, "요청한 경로를 찾을 수 없습니다."),
+    COMMON_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."),
+    GLOBAL_TEST_CODE(444, "테스트 컨트롤러의 코드입니다.");
 
     /*********************************************************************************/
 
     private final int code;
     private final String message;
 
-    ErrorType(@NonNull HttpStatus status, @NonNull String message) {
+    ErrorType(HttpStatus status, String message) {
         CodeValidation.validateErrorCode(status);
         this.code = status.value();
         this.message = message;
     }
 
-    ErrorType(int code, @NonNull String message) {
+    ErrorType(int code, String message) {
         CodeValidation.validateErrorCode(code);
         this.code = code;
         this.message = message;

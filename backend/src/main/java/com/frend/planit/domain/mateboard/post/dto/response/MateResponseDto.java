45b68a8e4ed2d21ee3e04e3187bf28b29acedff7
zelly-log <dearmyjaei@gmail.com>
2025-03-31 11:49:29 +0900

    [Refactor] Add Getter, Builder annotation

--- backend/src/main/java/com/frend/planit/domain/mateboard/post/dto/response/MateResponseDto.java
+++ backend/src/main/java/com/frend/planit/domain/mateboard/post/dto/response/MateResponseDto.java
@@ -5,6 +5,8 @@ import com.frend.planit.domain.mateboard.post.entity.RecruitmentStatus;
 import com.frend.planit.domain.mateboard.post.entity.TravelRegion;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
 
 /**
  * 메이트 모집 게시글 조회, 목록 응답 시에 사용하는 Response DTO입니다.
@@ -13,9 +15,11 @@ import java.time.LocalDateTime;
  * @version 1.0
  * @since 2025-03-28
  */
+@Getter
+@Builder
 public class MateResponseDto {
 
-    private Long matePostId;
+    private Long id;
 
     private String title;
 
@@ -32,4 +36,5 @@ public class MateResponseDto {
     private MateGender mateGender;
 
     private LocalDateTime createdAt;
+
 }

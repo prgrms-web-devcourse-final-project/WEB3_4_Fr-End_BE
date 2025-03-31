7be9b268b4f508daf71b5846a0eb42c99fcd80ef
HJ LEE <sete3683@gmail.com>
2025-03-31 16:43:10 +0900

    [Fix] 과도한 검증 로직 제거

--- backend/src/main/java/com/frend/planit/global/response/ApiResponseHelper.java
+++ backend/src/main/java/com/frend/planit/global/response/ApiResponseHelper.java
@@ -1,6 +1,5 @@
 package com.frend.planit.global.response;
 
-import com.frend.planit.global.validation.CodeValidation;
 import java.nio.charset.StandardCharsets;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
@@ -17,31 +16,26 @@ public class ApiResponseHelper<T> {
 
     /*
      * 성공 응답을 반환합니다.
-     * 성공 코드 범위가 아닐 시 IllegalArgumentException이 발생합니다.
      */
     public <T> ResponseEntity<T> success(@NonNull HttpStatus status) {
-        CodeValidation.validateSuccessCode(status);
         return ResponseEntity.status(status)
                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
                 .build();
     }
 
     public <T> ResponseEntity<T> success(@NonNull HttpStatus status, @NonNull T data) {
-        CodeValidation.validateSuccessCode(status);
         return ResponseEntity.status(status)
                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
                 .body(data);
     }
 
     public <T> ResponseEntity<T> success(int code) {
-        CodeValidation.validateSuccessCode(code);
         return ResponseEntity.status(code)
                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
                 .build();
     }
 
     public <T> ResponseEntity<T> success(int code, @NonNull T data) {
-        CodeValidation.validateSuccessCode(code);
         return ResponseEntity.status(code)
                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
                 .body(data);
@@ -50,17 +44,14 @@ public class ApiResponseHelper<T> {
     /*
      * 에러 응답을 반환합니다.
      * 에러는 핸들러에 의해 자동 처리되므로 특별한 경우가 아니면 사용을 지양해주세요.
-     * 에러 코드 범위가 아닐 시 IllegalArgumentException이 발생합니다.
      */
     public <T> ResponseEntity<T> error(@NonNull HttpStatus status, @NonNull T data) {
-        CodeValidation.validateErrorCode(status);
         return ResponseEntity.status(status)
                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
                 .body(data);
     }
 
     public <T> ResponseEntity<T> error(@NonNull int code, @NonNull T data) {
-        CodeValidation.validateErrorCode(code);
         return ResponseEntity.status(code)
                 .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
                 .body(data);

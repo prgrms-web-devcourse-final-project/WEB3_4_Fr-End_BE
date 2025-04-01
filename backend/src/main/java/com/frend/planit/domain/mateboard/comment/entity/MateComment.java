43b843494d10eb784b313b5999aa78da058d47d1
zelly-log <dearmyjaei@gmail.com>
2025-04-01 10:19:22 +0900

    [Feat] Add profileImageUrl

--- backend/src/main/java/com/frend/planit/domain/mateboard/comment/entity/MateComment.java
+++ backend/src/main/java/com/frend/planit/domain/mateboard/comment/entity/MateComment.java
@@ -12,7 +12,7 @@ import jakarta.persistence.ManyToOne;
  * 메이트 모집 게시판 댓글을 나타내는 엔티티입니다.
  * <p>댓글 수정 일자(modified_at)과 댓글 생성 날짜(created_at)는 BaseTime을 상속합니다.</p>
  * <p>
- * 댓글은 특정 메이트 모집 게시글에 작성되며, 작성자의 ID와 닉네임을 저장합니다.
+ * 댓글은 특정 메이트 모집 게시글에 작성됩니다.
  *
  * @author zelly
  * @version 1.0
@@ -37,7 +37,7 @@ public class MateComment extends BaseTime {
      *
      * @ManyToOne(fetch = FetchType.LAZY) private User id;
      */
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
     /**
@@ -45,6 +45,11 @@ public class MateComment extends BaseTime {
      */
     @Column(nullable = false)
     private String nickname;
+
+    /**
+     * 사용자 프로필 이미지 (TODO: User 엔티티와 연관 관계 매핑 예정)
+     */
+    private String profileImageUrl;
 
     /**
      * 댓글 내용

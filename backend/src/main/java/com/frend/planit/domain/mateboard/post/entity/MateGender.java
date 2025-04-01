6071284cd465f54d1c9261432b0c50a3fe34dee0
zelly-log <dearmyjaei@gmail.com>
2025-04-01 14:11:23 +0900

    [Refactor] @Getter, @RequiredArgsConstructor 대체

--- backend/src/main/java/com/frend/planit/domain/mateboard/post/entity/MateGender.java
+++ backend/src/main/java/com/frend/planit/domain/mateboard/post/entity/MateGender.java
@@ -1,5 +1,8 @@
 package com.frend.planit.domain.mateboard.post.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * 모집 메이트의 성별을 정의하는 Enum입니다. 여성(FEMALE), 남성(MALE), 성별 무관(NO_PREFERENCE) 3가지 항목이 존재합니다.
  *
@@ -7,6 +10,8 @@ package com.frend.planit.domain.mateboard.post.entity;
  * @version 1.0
  * @since 2025-03-28
  */
+@Getter
+@RequiredArgsConstructor
 public enum MateGender {
 
     NO_PREFERENCE("성별 무관"),
@@ -15,11 +20,4 @@ public enum MateGender {
 
     private final String displayName;
 
-    MateGender(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
 }

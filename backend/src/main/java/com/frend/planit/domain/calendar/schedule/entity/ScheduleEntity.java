da76e2356a7448f96259f09fb3925dd0760abf83
hellonaeunkim <hellonaeunkim@gmail.com>
2025-04-02 00:28:58 +0900

    feat : TravelEntity 생성 TravelEntity와 FK로 연결된 ScheduleEntity 추가

---
+++ backend/src/main/java/com/frend/planit/domain/calendar/schedule/entity/ScheduleEntity.java
@@ -0,0 +1,35 @@
+package com.frend.planit.domain.calendar.schedule.entity;
+
+import com.frend.planit.domain.calendar.schedule.travel.entity.TravelEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "schedule")
+public class ScheduleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<TravelEntity> travelList = new ArrayList<>();
+
+}

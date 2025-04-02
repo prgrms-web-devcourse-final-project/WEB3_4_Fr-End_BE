dcf8d706419a3317857b882a49afbcbfa94c6d5c
zelly-log <dearmyjaei@gmail.com>
2025-04-02 17:26:47 +0900

    [Feat] Add MateCommentRepository for comment CRUD functionality

--- backend/src/main/java/com/frend/planit/domain/mateboard/comment/repository/MateCommentRepository.java
+++ backend/src/main/java/com/frend/planit/domain/mateboard/comment/repository/MateCommentRepository.java
@@ -1,8 +1,12 @@
 package com.frend.planit.domain.mateboard.comment.repository;
 
 import com.frend.planit.domain.mateboard.comment.entity.MateComment;
+import com.frend.planit.domain.mateboard.post.entity.Mate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MateCommentRepository extends JpaRepository<MateComment, Long> {
 
+    Page<MateComment> findAllByMate(Mate mate, Pageable pageable);
 }

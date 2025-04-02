83a048065eb4a5df6266c2038f40c66c04f298ab
hellonaeunkim <hellonaeunkim@gmail.com>
2025-04-02 11:42:40 +0900

    fix : Repository 메서트 typo 수정

--- backend/src/main/java/com/frend/planit/domain/calendar/schedule/travel/repository/TravelRepository.java
+++ backend/src/main/java/com/frend/planit/domain/calendar/schedule/travel/repository/TravelRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TravelRepository extends JpaRepository<TravelEntity, Long> {
 
     // 특정 스케줄에 속한 모든 Travel 조회
-    List<TravelEntity> findBySheduleId(Long scheduleId);
+    List<TravelEntity> findByScheduleId(Long scheduleId);
 }

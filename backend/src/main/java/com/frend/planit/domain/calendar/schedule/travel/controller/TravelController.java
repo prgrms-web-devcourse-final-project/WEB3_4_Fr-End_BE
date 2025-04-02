3093f79b2149dda3adcf9f58a1ccc7320afde8d2
hellonaeunkim <hellonaeunkim@gmail.com>
2025-04-02 20:49:34 +0900

    refactor : ApiResponseHelper 클래스 삭제로 인한 Controller 응답코드 형태 리팩토링

--- backend/src/main/java/com/frend/planit/domain/calendar/schedule/travel/controller/TravelController.java
+++ backend/src/main/java/com/frend/planit/domain/calendar/schedule/travel/controller/TravelController.java
@@ -2,17 +2,16 @@ package com.frend.planit.domain.calendar.schedule.travel.controller;
 
 import com.frend.planit.domain.calendar.schedule.travel.dto.response.TravelResponse;
 import com.frend.planit.domain.calendar.schedule.travel.service.TravelService;
-import com.frend.planit.global.response.ApiResponseHelper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,11 +25,12 @@ public class TravelController {
     @GetMapping
     @SecurityRequirement(name = "bearerAuth")
     @Operation(summary = "전체 행선지 조회")
-    public ResponseEntity<List<TravelResponse>> getAllTravels(@PathVariable Long scheduleId) {
+    @ResponseStatus(HttpStatus.OK)
+    public List<TravelResponse> getAllTravels(@PathVariable Long scheduleId) {
 
         List<TravelResponse> travelResponses = travelService.getAllTravels(scheduleId);
 
-        return ApiResponseHelper.success(HttpStatus.OK, travelResponses);
+        return travelResponses;
     }
 
 }

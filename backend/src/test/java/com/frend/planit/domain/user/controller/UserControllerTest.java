b7121488096bbf19a419e5a0803a750d97e03ce7
c-hano <wdq0304@naver.com>
2025-04-02 13:17:42 +0900

    [FEAT] yml 파일 설정

--- backend/src/test/java/com/frend/planit/domain/user/controller/UserControllerTest.java
+++ backend/src/test/java/com/frend/planit/domain/user/controller/UserControllerTest.java
@@ -31,8 +31,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 class UserControllerTest {

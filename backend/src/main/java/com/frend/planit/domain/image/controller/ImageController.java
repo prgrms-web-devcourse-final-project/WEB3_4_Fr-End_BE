23db5a1c424954497ba7706f07378ae6ad6fbad3
HJ LEE <sete3683@gmail.com>
2025-04-04 15:43:43 +0900

    [Fix] api 엔드포인트 수정

--- backend/src/main/java/com/frend/planit/domain/image/controller/ImageController.java
+++ backend/src/main/java/com/frend/planit/domain/image/controller/ImageController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/image")
+@RequestMapping("/api/v1/image")
 public class ImageController {
 
     private final ImageService imageService;

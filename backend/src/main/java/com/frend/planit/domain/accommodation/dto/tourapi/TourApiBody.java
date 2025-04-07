2a6ef108b37e16093abdd0f64fc606e07ee5fe5b
kingyj00 <onshopyj@gmail.com>
2025-04-07 17:59:40 +0900

    [Feat] AddTourApiItems

--- backend/src/main/java/com/frend/planit/domain/accommodation/dto/tourapi/TourApiBody.java
+++ backend/src/main/java/com/frend/planit/domain/accommodation/dto/tourapi/TourApiBody.java
@@ -3,27 +3,23 @@ package com.frend.planit.domain.accommodation.dto.tourapi;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+import java.util.Collections;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TourApiBody {
 
-        private final List<TourApiItem> items;
+        private final TourApiItems items;
 
         @JsonCreator
-        public TourApiBody(
-                @JacksonXmlElementWrapper(localName = "items")
-                @JacksonXmlProperty(localName = "item")
-                @JsonProperty("items")
-                List<TourApiItem> items
-        ) {
+        public TourApiBody(@JsonProperty("items") TourApiItems items) {
                 this.items = items;
         }
 
         public List<TourApiItem> getItems() {
-                return items;
+                return items != null && items.item() != null
+                        ? items.item()
+                        : Collections.emptyList();
         }
 }

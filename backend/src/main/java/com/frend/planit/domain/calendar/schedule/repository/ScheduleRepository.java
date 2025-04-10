db25c9c7fd2a031a6b13a8948c524254ed257460
hellonaeunkim <hellonaeunkim@gmail.com>
2025-04-11 00:55:02 +0900

    refactor : service 리팩토링, repository 오류 수정

--- backend/src/main/java/com/frend/planit/domain/calendar/schedule/repository/ScheduleRepository.java
+++ backend/src/main/java/com/frend/planit/domain/calendar/schedule/repository/ScheduleRepository.java
@@ -2,11 +2,18 @@ package com.frend.planit.domain.calendar.schedule.repository;
 
 import com.frend.planit.domain.calendar.schedule.entity.ScheduleEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
+
+    // 스케줄 존재 여부 확인
+    @Query("SELECT s FROM ScheduleEntity s WHERE s.id = :scheduleId AND s.calendar.id = :calendarId")
+    Optional<ScheduleEntity> findByIdAndCalendarId(
+            @Param("calendarId") Long calendarId,
+            @Param("scheduleId") Long scheduleId);
 
     // 특정 유저의 모든 일정
     @Query("SELECT s FROM ScheduleEntity s WHERE s.calendar.user.id = :userId")

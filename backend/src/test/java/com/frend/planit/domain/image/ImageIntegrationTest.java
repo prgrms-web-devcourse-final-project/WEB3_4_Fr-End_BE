56e88c5ad599184aff189b490bb0ca061afe0eea
HJ LEE <sete3683@gmail.com>
2025-04-10 14:23:34 +0900

    [Fix] 적용 Profile 변경

--- backend/src/test/java/com/frend/planit/domain/image/ImageIntegrationTest.java
+++ backend/src/test/java/com/frend/planit/domain/image/ImageIntegrationTest.java
@@ -22,14 +22,14 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-@Profile("test")
+@ActiveProfiles("test")
 @SpringBootTest()
 @AutoConfigureMockMvc
 @Transactional
